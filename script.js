d39e8ba177e189ac7f2065f773622ae1ea519125
KJ <irateboolean@gmx.com>
2021-10-07 18:46:27 -0400

    added color toggle

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 const container = document.querySelector('.container')
 
+function changeTile(){
+  this.classList.toggle("alt")
+}
+
+
+
 
 for (i = 0; i < 16; i++) {
   for (j = 0; j < 16; j++) {
 
     const box = document.createElement('div')
-box.classList.add('box')
+
+    box.classList.add('box')
+    box.addEventListener('mouseover', changeTile)
     container.appendChild(box)
   }
 }
